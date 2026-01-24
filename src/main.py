a951b2c1f8cad1f576ff10bce87e5e05c2151ad1
luqastw <lucasborcem1@gmail.com>
2026-01-23 21:16:24 -0300

    chore: users route included in main

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.core.config import settings
-from src.api.routes import auth
+from src.api.routes import auth, users
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -25,6 +25,7 @@ app.add_middleware(
 )
 
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
 
 
 @app.get(
